88b7158b90d735736a27e8c45814f7441f858961
Alan Smith <alan@randomsmiths.com>
2024-12-11 13:10:50 -0500

    Day 10, pt 2

--- 10/main.py
+++ 10/main.py
@@ -8,24 +8,24 @@ max_y = 0
 
 @functools.lru_cache(maxsize=None)
 def find_path(x, y):
-    end_pts = set()
+    end_pts = []
     current_height = topo_map[y][x]
 
     if current_height == 9:
-        end_pts.add((x, y))
+        end_pts.append((x, y))
         return end_pts
     if x > 0:
         if topo_map[y][x - 1] == current_height + 1:
-            end_pts.update(find_path(x - 1, y))
+            end_pts.extend(find_path(x - 1, y))
     if y > 0:
         if topo_map[y - 1][x] == current_height + 1:
-            end_pts.update(find_path(x, y - 1))
+            end_pts.extend(find_path(x, y - 1))
     if x < (max_x - 1):
         if topo_map[y][x + 1] == current_height + 1:
-            end_pts.update(find_path(x + 1, y))
+            end_pts.extend(find_path(x + 1, y))
     if y < (max_y - 1):
         if topo_map[y + 1][x] == current_height + 1:
-            end_pts.update(find_path(x, y + 1))
+            end_pts.extend(find_path(x, y + 1))
     return end_pts
 
 
